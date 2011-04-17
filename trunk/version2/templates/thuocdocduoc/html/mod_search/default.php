dd39074f3046508710d0031f2e0c0b57d9b2d6be
longilx <longilx@92ad57e5-38ed-5e59-6a6f-df579c2e92c2>
2011-04-17 06:12:12 +0000

    fix search + add google search

--- trunk/version2/templates/thuocdocduoc/html/mod_search/default.php
+++ trunk/version2/templates/thuocdocduoc/html/mod_search/default.php
@@ -8,6 +8,10 @@ $sections = array_unique(explode(',',$sections));
 $db = &JFactory::getDBO();
 $db->setQuery("SELECT id,title FROM #__sections WHERE id IN (".implode(',',$sections).")");
 $list = $db->loadObjectList();
+$objGoogle = new stdClass();
+$objGoogle->id ='google';
+$objGoogle->title ='Google';
+$list[] = $objGoogle;
 //var_dump($list);
 ?>
 <div>
@@ -15,13 +19,13 @@ $list = $db->loadObjectList();
 <div>
 	<h3 >Tìm kiếm</h3>
 <div class="form" style="padding-bottom:12px;">
-<form action="index.php" method="post">
+<form action="index.php" method="post" id="cse-search-box">
     <label>
         Từ khóa</label>
     <input type="text" class="input" name="searchword" value="<?php echo JRequest::getVar('searchword');?>">
     <label>
         Loại tìm kiếm</label>
-    <select name="sectionid" style="margin-bottom:15px;">
+    <select name="sectionid" style="margin-bottom:15px;" id="select_sectionid">
 		<option value="<?php echo implode(',',$sections);?>">Tất cả</option>
         <?php foreach($list as $item):?>			
 			<option value="<?php echo $item->id;?>" <?php echo $item->id==JRequest::getVar('sectionid')?"selected":'';?>><?php echo ucfirst($item->title);?></option>
@@ -34,6 +38,9 @@ $list = $db->loadObjectList();
 	<input type="hidden" name="areas[]"   value="content" />
 	<input type="hidden" name="option" value="com_search" />
 	<input type="hidden" name="limit" value="20" />
+	<input type="hidden" name="cx" value="partner-pub-6986041461827618:8269881805" />
+    <input type="hidden" name="ie" value="UTF-8" />
+    <input type="hidden" name="sa" value="Search" />
 <!--	<input type="hidden" name="Itemid" value=<?php echo $mitemid; ?> />-->
 </form>
 </div>
