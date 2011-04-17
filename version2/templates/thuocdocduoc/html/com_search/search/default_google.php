eb6e492b7f7c11209a4724c380e9c3d7ba32f1a0
longilx <longilx@92ad57e5-38ed-5e59-6a6f-df579c2e92c2>
2011-04-17 06:33:30 +0000

    finished search

--- version2/templates/thuocdocduoc/html/com_search/search/default_google.php
+++ version2/templates/thuocdocduoc/html/com_search/search/default_google.php
@@ -14,17 +14,24 @@ JPluginHelper::importPlugin('content');
         <img src="<?php echo $templateUrl;?>/images/news&event_107.png" />
     </div>
     <div class="cnt">
-		<script src="//www.google.com/jsapi" type="text/javascript"></script>
-		<script type="text/javascript"> 
-		  google.load('search', '1', {language : 'vi'});
-		  google.setOnLoadCallback(function() {
-			var customSearchControl = new google.search.CustomSearchControl('016946892290065192097:rx_ut3g3uha');
-			customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
-			customSearchControl.draw('cse');
-		  }, true);
-		</script>
-		<link rel="stylesheet" href="//www.google.com/cse/style/look/default.css" type="text/css" />
+		<div id="cse" style="width: 100%;">Loading</div>
+			<script src="//www.google.com/jsapi" type="text/javascript"></script>
+			<script type="text/javascript"> 
+			  google.load('search', '1', {language : 'vi'});
+			  google.setOnLoadCallback(function() {
+				var customSearchControl = new google.search.CustomSearchControl('016946892290065192097:rx_ut3g3uha');
+				customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
+				customSearchControl.draw('cse');
+				jQuery('#cse').find('input[name=search]').val('<?php echo $this->searchword;?>');
+				jQuery('#cse').find('input[name=search]').click();
+				jQuery('#cse').find('input.gsc-search-button').click();
+				
+			  }, true);
+			</script>
+			<link rel="stylesheet" href="//www.google.com/cse/style/look/default.css" type="text/css" />
     </div>
     <img src="<?php echo $templateUrl;?>/images/news&event_73.png" class="img-rounded" />
 </div>
+<script language="javascript">
+</script>
 <!--end-mdl-3-->
